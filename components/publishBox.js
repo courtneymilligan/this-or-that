5e5a2e76639899996c7623a2f06c18cb1124e099
courtneymilligan <courtneymilligan1@gmail.com>
2021-05-07 14:49:28 -0700

    send email funcationality

--- components/publishBox.js
+++ components/publishBox.js
@@ -1,19 +1,50 @@
 import publishBoxStyles from "../styles/PublishBox.module.css";
 import homeStyles from "../styles/Home.module.css";
 import html2canvas from 'html2canvas';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createRef } from "react";
 
 export default function PublishBox({ image1, image2, text }) {
-    const [count, setCount] = useState(0); 
-    const contentRef = React.useRef();
-    useEffect(() => {
-        /*const contentNode = contentRef.current;
-        html2canvas(contentNode).then(function(canvas) {
-            document.body.appendChild(canvas);
-        });*/
-        html2canvas(document.body).then(canvas => {
-            console.log("screenshot captured")})
-    }, [count]);
+    const ref = createRef();
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      /*if (event.target.elements.email.value === "") {
+          alert("Please enter an email address");
+          return;
+      } else if (!image1 || !image2) {
+          alert("Make sure both images are selected");
+          return;
+      } else if (!text.prompt) {
+          alert("You forgot to input a prompt");
+          return;
+      }*/
+      const contentNode = ref.current;
+      const email = event.target.elements.email.value;
+      html2canvas(contentNode, {
+        useCORS: true,
+        allowTaint: true,
+        backgroundColor: "#25272b",
+        scrollY: -window.scrollY,
+        scrollX: -window.scrollX - 5
+      }).then(function(canvas) {
+        const image = canvas.toDataURL("image/png");
+        Email.send({
+          SecureToken : process.env.NEXT_PUBLIC_EMAIL_TOKEN,
+          To : email,
+          From : process.env.NEXT_PUBLIC_EMAIL_ADDRESS,
+          Subject : "This or That!",
+          Body : "Test",
+          Attachments : [
+            {
+              name : "this-or-that.png",
+              path : "https://www.sciencemag.org/sites/default/files/styles/inline__450w__no_aspect/public/dogs_1280p_0.jpg?itok=h6VBayx-"
+            }]
+        });
+        document.getElementById("screenshot").src = image;
+        alert("Your message has been sent to " + email);
+      });
+      //event.target.elements.email.value = "";
+  };
 
     return (
       <div className={homeStyles.container}>
@@ -21,16 +52,19 @@ export default function PublishBox({ image1, image2, text }) {
         <div className={publishBoxStyles.contentContainer}>
             <div className={publishBoxStyles.previewCase}>
                 <p className={publishBoxStyles.preview}>Preview of Selections</p>
-                <div ref={contentRef} id="content" className={publishBoxStyles.previewContent}>
-                    <p className={publishBoxStyles.prompt}>{text.prompt}</p>
-                    <div className={publishBoxStyles.imageCase}>
-                        <ImageDisplay number="First" image={image1} label={text.label1}/>
-                        <div className={publishBoxStyles.or}>OR</div>
-                        <ImageDisplay number="Second" image={image2} label={text.label2}/>
-                    </div>
+                <div id="content" className={publishBoxStyles.previewContent}>
+                  <div ref={ref}>
+                      <p className={publishBoxStyles.prompt}>{text.prompt}</p>
+                      <div className={publishBoxStyles.imageCase}>
+                          <ImageDisplay number="First" image={image1} label={text.label1}/>
+                          <div className={publishBoxStyles.or}>OR</div>
+                          <ImageDisplay number="Second" image={image2} label={text.label2}/>
+                      </div>
+                      </div>
                 </div>   
             </div>  
-            <PublishForm image1={image1} image2={image2} text={text} count={count} setCount={setCount}/>   
+            <PublishForm handleSubmit={handleSubmit}/>
+            <img id="screenshot" width="300px"></img>
         </div>
       </div>
     );
@@ -39,7 +73,7 @@ export default function PublishBox({ image1, image2, text }) {
 function ImageDisplay( {number, image, label} ) {
     return (
         <div className={publishBoxStyles.imageContainer}>
-            <p className={publishBoxStyles.textContainer}>{`${number} Image Not Selected`}</p>
+            <p className={publishBoxStyles.textContainer}>{image ? null : `${number} Image Not Selected`}</p>
             {image ? <img src={image.link} alt={image.title}></img> : null}
             <p className={publishBoxStyles.labels}>{label}</p>
         </div>
@@ -47,34 +81,7 @@ function ImageDisplay( {number, image, label} ) {
     
 }
 
-function PublishForm({image1, image2, text, count, setCount}) { 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        /*if (event.target.elements.email.value === "") {
-            alert("Please enter an email address");
-            return;
-        } else if (!image1 || !image2) {
-            alert("Make sure both images are selected");
-            return;
-        } else if (!text.prompt) {
-            alert("You forgot to input a prompt");
-            return;
-        }*/
-        setCount(count + 1);
-
-        /*
-        const body = "Test";
-        Email.send({
-            SecureToken : process.env.NEXT_PUBLIC_EMAIL_TOKEN,
-            To : event.target.elements.email.value,
-            From : process.env.NEXT_PUBLIC_EMAIL_ADDRESS,
-            Subject : "This or That!",
-            Body : body,
-        })
-        alert("Your message has been sent to " + event.target.elements.email.value);*/
-        event.target.elements.email.value = "";
-    };
-  
+function PublishForm({handleSubmit}) { 
     return (
       <form className={publishBoxStyles.sendBox} onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
